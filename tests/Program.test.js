dcb42af60b5da5900c0d9aaf0db6b83aa50a61ad
Zhuofeng Wu <1844044@students.wits.ac.za>
2022-06-03 16:04:40 +0200

    Added more Test
    -Added DataBase Tests

--- tests/Program.test.js
+++ tests/Program.test.js
@@ -2,9 +2,66 @@
 const request = require("supertest");
 const express = require("express");
 let app = require("../index")
+const UIDLib = require('../scripts/HashGen')
+const DB = require("../scripts/dbInteraction")
 
 beforeAll(() =>{
   app.close();
+})
+
+//==============================================================================\\
+//Route Tests
+//==============================================================================\\
+describe('Can Reach Game Page', function () {
+  test('responds to /Game', async () => {
+    const res = await request(app).get('/Game')
+    expect(res.statusCode).toBe(200);
+  });
+})
+
+
+describe('Can Reach Home Page', function () {
+  test('responds to /', async () => {
+    const res = await request(app).get('/')
+    expect(res.statusCode).toBe(200);
+  });
+})
+
+describe('Can Reach Queue Page', function () {
+  test('responds to /Queue', async () => {
+    const res = await request(app).get('/Queue')
+    expect(res.statusCode).toBe(200);
+  });
+})
+
+
+
+//==============================================================================\\
+//Hash test
+//==============================================================================\\
+
+describe('Hash Test', function () {
+  test('Can Generate Hash', async () => { 
+    expect(UIDLib.GenerateUniqueHash("Ivan")).toBe(2291258)
+  });
+})
+
+//==============================================================================\\
+//Login
+//==============================================================================\\
+
+describe('Can login', function () {
+  test('responds to /Auth/AddNewConnection', async () => {
+    const res = await request(app).post('/Login/Login').send({playerName: "admin", userPassword: "admin"});
+    expect(res.body).toBe(3)
+  });
+})
+
+describe('Cant login', function () {
+  test('responds to /Auth/AddNewConnection', async () => {
+    const res = await request(app).post('/Login/Login').send({playerName: "admin", userPassword: "nonadmin"});
+    expect(res.body).toBe("FAILED")
+  });
 })
 
 //==============================================================================\\
@@ -92,7 +149,7 @@ describe('Can get word', function () {
   test('responds to /Game/GetWord', async () => {
     const res = await request(app).get('/Game/GetWord')
     expect(res.statusCode).toBe(200);
-    expect(res.body).toBe("")
+    expect(res.body).not.toBe("")
   });
 })
 
@@ -103,6 +160,70 @@ describe('Can get game Mode', function () {
     expect(res.body.gameMode).toBe(false)
   });
 })
+
+
+//==============================================================================\\
+//DataBase Tests
+//==============================================================================\\
+
+describe('Database Test', function() {
+  jest.setTimeout(10000);
+  test('Can get a random word from the database', async () =>{
+    const randomWord = await DB.getRandomWord()
+
+    const ID = await DB.IsLegalWord(randomWord.Word)
+
+    expect(ID).toBe(randomWord.WordID)
+  })
+})
+
+describe('Database Test', function() {
+  jest.setTimeout(10000);
+  test('Can check for legal word', async () =>{
+    const legalWord = 'after'
+
+    const ID = await DB.IsLegalWord(legalWord)
+
+    expect(ID).not.toBe(-1)
+  })
+})
+
+describe('Database Test', function() {
+  jest.setTimeout(15000);
+  test('Can register a player and record it in the database', async () =>{
+    const testAccount = 'TestAccount'
+    const testPassword = 'HashedPassWord'
+    
+    const registeredID = await DB.RegisterPlayer(testAccount, testPassword)
+
+    const logInID = await DB.PlayerLogin(testAccount, testPassword)
+
+    await DB.DeletePlayerByID(registeredID)
+
+    expect(registeredID).toBe(logInID)
+  })
+})
+
+describe('Database Test', function() {
+  jest.setTimeout(15000);
+  test('Can a player login', async () =>{
+    const testAccount = 'LogTestAccount'
+    const testPassword = 'HashedPassWord'
+    
+    const registeredID = await DB.RegisterPlayer(testAccount, testPassword)
+
+    const logInID = await DB.PlayerLogin(testAccount, testPassword)
+
+    await DB.DeletePlayerByID(registeredID)
+
+    expect(registeredID).toBe(logInID)
+  })
+})
+
+//==============================================================================\\
+
+
+
 
 beforeEach(() => {
   app = require("../index")
